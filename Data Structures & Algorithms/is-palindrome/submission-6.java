272ef2e97a32b92a301b5f8d38d2f80fc2f957d0
Nined Somaiya <nineds24@gmail.com>
2026-07-24 16:04:05 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] c = s.toCharArray();
+        List<Character> temp = new ArrayList<>();
+        ArrayList<Character> chars = new ArrayList<>(List.of('a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'));
+        for(int i=0;i<c.length;i++){
+            if(chars.contains(c[i])){
+                temp.add(Character.toLowerCase(c[i]));
+            }   
+        }
+        int i=0,j=temp.size()-1;
+        System.out.println(temp);
+        System.out.println(j);
+        while(j-i>=1){
+            if(temp.get(i)!=temp.get(j)){
+                System.out.print(temp.get(i)+" "+temp.get(j)+j);
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
